4ea2eb0d14ffef8b4ea7f44b6ec254cd878543c5
mcxu233 <mcxu233@gmail.com>
2017-11-30 18:48:54 +0800

    fix

--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
 $(function () {
     $('#dowebok').fullpage({
-<<<<<<< HEAD
         autoScrolling: false
-=======
-        navigation: true
->>>>>>> 67debcc0d7c06476d414320a08ad1068ee94a7b5
     });
 });
