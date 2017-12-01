c9a56befd805e2da2e7a421b64a099694b5f898f
mcxu233 <mcxu233@gmail.com>
2017-12-01 20:05:22 +0800

    fix page1 and page2

--- js/index.js
+++ js/index.js
@@ -2,4 +2,8 @@ $(function () {
     $('#dowebok').fullpage({
         autoScrolling: false
     });
+
+    $(".skill-list").hover(function () {
+        $(this).find(".skill-detail").fadeToggle(200);
+    });
 });
